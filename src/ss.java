61e74ba11ca5ff4ea9ae0fa8e6181af9932d2a45
Administrator <666@163.com>
2017-07-12 14:38:21 +0800

    the thrid commit

--- src/ss.java
+++ src/ss.java
@@ -2,8 +2,11 @@
  * Created by Administrator on 2017/7/11 0011.
  */
 public class ss {
-    int a=0;
-
+    static int a=0;
+    static int b=1;
+    public static int sum(){
+        return a+b;
+    }
     public void setA(int a) {
         this.a = a;
     }
